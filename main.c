e23c35fb8ece7bdaac4b44b250d7432e9bd6be83
tommykey0925 <tommykey0925@gmail.com>
2026-02-11 21:42:19 +0900

    fix many things

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 # define RESET "\x1b[0m"
 
 int main(void) {
-  char ln1[64] = "";
-  char ln2[64] = "";
-  char ln3[64] = "";
-  char ln4[64] = "";
-  char ln5[64] = "======|" RED "=" RESET;
+  char ln1[64] = "        *\n";
+  char ln2[64] = "        *\n";
+  char ln3[64] = "        *\n";
+  char ln4[64] = "        *\n";
+  char ln5[64] = "======" RED "=" RESET;
 
+  printf(ln1);
+  printf(ln2);
+  printf(ln3);
+  printf(ln4);
   printf(ln5);
   return 0;
 }
